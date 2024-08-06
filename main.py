6e8f4e5292c11177637365958bdad1bdecd5f736
Hebert Garcia da Silva <113214587+HebertGarcia@users.noreply.github.com>
2024-08-06 07:02:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 from io import BytesIO
 
-def processar_arquivo(uploaded_file, file_type, tipo_glosa):
+def processar_arquivo_glosa_inicial(uploaded_file, file_type):
     try:
         if file_type == 'xlsb':
             df = pd.read_excel(uploaded_file, engine='pyxlsb')
@@ -14,23 +14,19 @@ def processar_arquivo(uploaded_file, file_type, tipo_glosa):
             st.error("Tipo de arquivo não suportado!")
             return None
 
-        if tipo_glosa == 'inicial':
-            colunas_necessarias = ['Marca', 'Operadora', 'Motivo Operadora (Código)', 'Glosa Inicial', 'Desc. Proced. DCM']
-        else:
-            colunas_necessarias = ['Marca', 'Operadora', 'Motivo Glosa Operadora (Código)', 'Glosa Aceita', 'Procedimento (Descrição)']
-
+        colunas_necessarias = ['Marca', 'Operadora', 'Motivo Operadora (Código)', 'Glosa Inicial', 'Desc. Proced. DCM']
         if not all(col in df.columns for col in colunas_necessarias):
             st.error("O arquivo não contém as colunas necessárias!")
             return None
 
-        colunas = colunas_necessarias[:4]
+        colunas = ['Marca', 'Operadora', 'Motivo Operadora (Código)', 'Glosa Inicial', 'Desc. Proced. DCM']
         df_selecionado = df[colunas]
 
-        df_somatorio = df_selecionado.groupby(colunas[:3])[
-            [colunas[3]]
+        df_somatorio = df_selecionado.groupby(['Marca', 'Operadora', 'Motivo Operadora (Código)', 'Desc. Proced. DCM'])[
+            ['Glosa Inicial']
         ].sum().reset_index()
 
-        df_somatorio = df_somatorio.sort_values(by=colunas[:3])
+        df_somatorio = df_somatorio.sort_values(by=['Marca', 'Operadora', 'Motivo Operadora (Código)'])
 
         def formatar_valor(valor):
             if valor >= 1e6:
@@ -41,8 +37,8 @@ def processar_arquivo(uploaded_file, file_type, tipo_glosa):
                 return f"{valor:.0f}"
 
         def formatar_linha(row, contador):
-            valor_formatado = formatar_valor(row[colunas[3]])
-            return f"{contador} - {row[colunas[2]]} ({valor_formatado})"
+            valor_formatado = formatar_valor(row['Glosa Inicial'])
+            return f"{contador} - {row['Desc. Proced. DCM']} ({valor_formatado})"
 
         def linha_completa(dados):
             dados.reset_index(drop=True, inplace=True)
@@ -52,29 +48,113 @@ def processar_arquivo(uploaded_file, file_type, tipo_glosa):
 
         df_principal = pd.DataFrame(columns=['Unidade', 'Operadora', 'Código de Glosa', 'Valor Glosado', 'Ofensores (TOP 5)'])
 
-        df_somatorio_marca = df_selecionado.groupby(colunas[:3])[
-            [colunas[3]]
+        colunas = ['Marca', 'Operadora', 'Motivo Operadora (Código)', 'Glosa Inicial']
+        df_selecionado2 = df[colunas]
+
+        df_somatorio_marca = df_selecionado2.groupby(['Marca', 'Operadora', 'Motivo Operadora (Código)'])[
+            ['Glosa Inicial']
         ].sum().reset_index()
 
-        df_soma_operadora = df_selecionado.groupby(colunas[:2])[
-            [colunas[3]]
+        df_soma_operadora = df_selecionado2.groupby(['Marca', 'Operadora'])[
+            ['Glosa Inicial']
         ].sum().reset_index()
 
-        df_somatorio_marca = df_somatorio_marca.sort_values(by=colunas[:3])
+        df_somatorio_marca = df_somatorio_marca.sort_values(by=['Marca', 'Operadora', 'Motivo Operadora (Código)'])
 
         lista_marcas = df_somatorio_marca['Marca'].unique()
 
         for marca in lista_marcas:
-            df_1marca = df_soma_operadora[df_soma_operadora['Marca'] == marca].sort_values(colunas[3], ascending=False)
-            df_marca_codigo = df_somatorio_marca[df_somatorio_marca['Marca'] == marca].sort_values(colunas[3], ascending=False)
+            df_1marca = df_soma_operadora[df_soma_operadora['Marca'] == marca].sort_values('Glosa Inicial', ascending=False)
+            df_marca_codigo = df_somatorio_marca[df_somatorio_marca['Marca'] == marca].sort_values('Glosa Inicial', ascending=False)
             lista_operadoras = df_1marca['Operadora'].unique()
             for operadora in lista_operadoras:
-                df_1operadora = df_marca_codigo[df_marca_codigo['Operadora'] == operadora].sort_values(colunas[3], ascending=False)[:8]
-                codigos = df_1operadora[colunas[2]].unique()
+                df_1operadora = df_marca_codigo[df_marca_codigo['Operadora'] == operadora].sort_values('Glosa Inicial', ascending=False)[:8]
+                codigos = df_1operadora['Motivo Operadora (Código)'].unique()
                 for codigo_ in codigos:
-                    df_top5_dcm_ = df_somatorio.query(f"Operadora == '{operadora}' and Marca == '{marca}' and `{colunas[2]}` =='{codigo_}' ").sort_values(colunas[3], ascending=False).reset_index()[:5]
+                    df_top5_dcm_ = df_somatorio.query(f"Operadora == '{operadora}' and Marca == '{marca}' and `Motivo Operadora (Código)` =='{codigo_}' ").sort_values('Glosa Inicial', ascending=False).reset_index()[:5]
                     texto = linha_completa(df_top5_dcm_)
-                    soma_total = df_somatorio_marca.query(f"Operadora == '{operadora}' and Marca == '{marca}' and `{colunas[2]}` =='{codigo_}'")[colunas[3]].values[0]
+                    soma_total = df_somatorio_marca.query(f"Operadora == '{operadora}' and Marca == '{marca}' and `Motivo Operadora (Código)` =='{codigo_}'")['Glosa Inicial'].values[0]
+                    dados = [marca, operadora, codigo_, soma_total, texto]
+                    novo_df = pd.DataFrame([dados], columns=df_principal.columns)
+                    df_principal = pd.concat([df_principal, novo_df], ignore_index=True)
+
+        return df_principal
+    except Exception as e:
+        st.error(f"Erro ao processar o arquivo: {e}")
+        return None
+
+def processar_arquivo_glosa_aceita(uploaded_file, file_type):
+    try:
+        if file_type == 'xlsb':
+            df = pd.read_excel(uploaded_file, engine='pyxlsb')
+        elif file_type == 'xlsx':
+            df = pd.read_excel(uploaded_file)
+        elif file_type == 'csv':
+            df = pd.read_csv(uploaded_file)
+        else:
+            st.error("Tipo de arquivo não suportado!")
+            return None
+
+        colunas_necessarias = ['Marca', 'Operadora', 'Motivo Glosa Operadora (Código)', 'Glosa Aceita', 'Procedimento (Descrição)']
+        if not all(col in df.columns for col in colunas_necessarias):
+            st.error("O arquivo não contém as colunas necessárias!")
+            return None
+
+        colunas = ['Marca', 'Operadora', 'Motivo Glosa Operadora (Código)', 'Glosa Aceita', 'Procedimento (Descrição)']
+        df_selecionado = df[colunas]
+
+        df_somatorio = df_selecionado.groupby(['Marca', 'Operadora', 'Motivo Glosa Operadora (Código)', 'Procedimento (Descrição)'])[
+            ['Glosa Aceita']
+        ].sum().reset_index()
+
+        df_somatorio = df_somatorio.sort_values(by=['Marca', 'Operadora', 'Motivo Glosa Operadora (Código)'])
+
+        def formatar_valor(valor):
+            if valor >= 1e6:
+                return f"{valor/1e6:.2f}M"
+            elif valor >= 1e3:
+                return f"{valor/1e3:.2f}k"
+            else:
+                return f"{valor:.0f}"
+
+        def formatar_linha(row, contador):
+            valor_formatado = formatar_valor(row['Glosa Aceita'])
+            return f"{contador} - {row['Procedimento (Descrição)']} ({valor_formatado})"
+
+        def linha_completa(dados):
+            dados.reset_index(drop=True, inplace=True)
+            dados['String Formatada'] = dados.apply(lambda row: formatar_linha(row, row.name + 1), axis=1)
+            todas_as_linhas = '\n'.join(dados['String Formatada'])
+            return todas_as_linhas
+
+        df_principal = pd.DataFrame(columns=['Unidade', 'Operadora', 'Código de Glosa', 'Valor Glosado', 'Ofensores (TOP 8)'])
+
+        colunas = ['Marca', 'Operadora', 'Motivo Glosa Operadora (Código)', 'Glosa Aceita']
+        df_selecionado2 = df[colunas]
+
+        df_somatorio_marca = df_selecionado2.groupby(['Marca', 'Operadora', 'Motivo Glosa Operadora (Código)'])[
+            ['Glosa Aceita']
+        ].sum().reset_index()
+
+        df_soma_operadora = df_selecionado2.groupby(['Marca', 'Operadora'])[
+            ['Glosa Aceita']
+        ].sum().reset_index()
+
+        df_somatorio_marca = df_somatorio_marca.sort_values(by=['Marca', 'Operadora', 'Motivo Glosa Operadora (Código)'])
+
+        lista_marcas = df_somatorio_marca['Marca'].unique()
+
+        for marca in lista_marcas:
+            df_1marca = df_soma_operadora[df_soma_operadora['Marca'] == marca].sort_values('Glosa Aceita', ascending=False)
+            df_marca_codigo = df_somatorio_marca[df_somatorio_marca['Marca'] == marca].sort_values('Glosa Aceita', ascending=False)
+            lista_operadoras = df_1marca['Operadora'].unique()
+            for operadora in lista_operadoras:
+                df_1operadora = df_marca_codigo[df_marca_codigo['Operadora'] == operadora].sort_values('Glosa Aceita', ascending=False)[:8]
+                codigos = df_1operadora['Motivo Glosa Operadora (Código)'].unique()
+                for codigo_ in codigos:
+                    df_top5_dcm_ = df_somatorio.query(f"Operadora == '{operadora}' and Marca == '{marca}' and `Motivo Glosa Operadora (Código)` =='{codigo_}' ").sort_values('Glosa Aceita', ascending=False).reset_index()[:5]
+                    texto = linha_completa(df_top5_dcm_)
+                    soma_total = df_somatorio_marca.query(f"Operadora == '{operadora}' and Marca == '{marca}' and `Motivo Glosa Operadora (Código)` =='{codigo_}'")['Glosa Aceita'].values[0]
                     dados = [marca, operadora, codigo_, soma_total, texto]
                     novo_df = pd.DataFrame([dados], columns=df_principal.columns)
                     df_principal = pd.concat([df_principal, novo_df], ignore_index=True)
@@ -94,13 +174,12 @@ def convert_df_to_excel(df):
 def main():
     st.title("Plano de Ação Glosas - Desenvolvido por Hebert Garcia")
 
-    st.sidebar.header("Upload de Arquivos")
-    st.sidebar.subheader("Glosa Inicial")
-    uploaded_file_inicial = st.sidebar.file_uploader("Arquivo de Glosa Inicial", type=["xlsb", "xlsx", "csv"], key="glosa_inicial")
+    st.header("Glosa Inicial")
+    uploaded_file_inicial = st.file_uploader("Faça upload do arquivo Excel de Glosa Inicial", type=["xlsb", "xlsx", "csv"], key="glosa_inicial")
     if uploaded_file_inicial is not None:
         file_type_inicial = uploaded_file_inicial.name.split('.')[-1]
-        if st.sidebar.button("Processar Glosa Inicial"):
-            df_resultado_inicial = processar_arquivo(uploaded_file_inicial, file_type_inicial, 'inicial')
+        if st.button("Processar Arquivo Glosa Inicial"):
+            df_resultado_inicial = processar_arquivo_glosa_inicial(uploaded_file_inicial, file_type_inicial)
             if df_resultado_inicial is not None:
                 st.write("Arquivo de Glosa Inicial processado com sucesso!")
                 st.write(df_resultado_inicial)
@@ -112,12 +191,12 @@ def main():
                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                 )
 
-    st.sidebar.subheader("Glosa Aceita")
-    uploaded_file_aceita = st.sidebar.file_uploader("Arquivo de Glosa Aceita", type=["xlsb", "xlsx", "csv"], key="glosa_aceita")
+    st.header("Glosa Aceita")
+    uploaded_file_aceita = st.file_uploader("Faça upload do arquivo Excel de Glosa Aceita", type=["xlsb", "xlsx", "csv"], key="glosa_aceita")
     if uploaded_file_aceita is not None:
         file_type_aceita = uploaded_file_aceita.name.split('.')[-1]
-        if st.sidebar.button("Processar Glosa Aceita"):
-            df_resultado_aceita = processar_arquivo(uploaded_file_aceita, file_type_aceita, 'aceita')
+        if st.button("Processar Arquivo Glosa Aceita"):
+            df_resultado_aceita = processar_arquivo_glosa_aceita(uploaded_file_aceita, file_type_aceita)
             if df_resultado_aceita is not None:
                 st.write("Arquivo de Glosa Aceita processado com sucesso!")
                 st.write(df_resultado_aceita)
